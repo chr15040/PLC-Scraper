8c67835713ca36144e62102bb99e47e19e90b84a
Christian Khoury <ckhoury@esri.com>
2026-03-18 09:27:14 -0700

    Scrape whole page

--- src/plc-scraper.py
+++ src/plc-scraper.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 from playwright.async_api import async_playwright
 from playwright_stealth import Stealth
 
-from utils.scraper_utils import get_date_tag, get_product_tag, make_metadata
+from utils.scraper_utils import *
 
 url = "https://support.esri.com/en-us/products/arcgis-pro/life-cycle"
 
@@ -28,21 +28,20 @@ async def fetch_url_source(url):
 def get_content(soup):
     title = soup.find("h1", class_="esri-text__title")
     subtitle = soup.find("h2", class_="esri-text__title")
-    # print(title)
-    # print(subtitle)
+    
+    table1 = convert_tech_supt_table(soup)
 
-    table1 = soup.find("div", class_="technical-support-table")
-    table = table1.find("div", class_="table-wrapper")
-    table1 = convert_table(table.table)
+    table2 = prune_version_table(soup)
 
-
-    table2 = soup.find("div", class_="multiple-version-table")
-    table2 = prune_version_table(table2)
+    add_prod_info = get_additional_prod_info(soup)
+    
+    content = f"{title} {subtitle} {table1} {table2} {add_prod_info}"
+    print(content)
 
     metadata = get_metadata(soup, title)
 
 
-def convert_table(table: BeautifulSoup):
+def convert_tech_supt_table(soup: BeautifulSoup):
     """
     Convert a technical support table from the Esri support site into a simplified list of bullet points in the form:
     
@@ -51,6 +50,9 @@ def convert_table(table: BeautifulSoup):
     • Mature June 01, 2027—November 30, 2028: Create case, online support
     • Retirement December 01, 2028: online support
     """
+    table_div = soup.find("div", class_="technical-support-table")
+    table = table_div.find("div", class_="table-wrapper")
+
     caption = table.caption
     row_headers = [tag.get_text().strip() for tag in table.tbody.find_all("th")]
     row_contents = [[content for content in row.find_all("td")] for row in table.tbody.find_all("tr")]
@@ -73,11 +75,14 @@ def convert_table(table: BeautifulSoup):
     return result
 
 
-def prune_version_table(table: BeautifulSoup):
+def prune_version_table(soup: BeautifulSoup):
     """
     Prune the html of the version table from the support site to exclude all color and styling info and any retired versions
     """
+    table = soup.find("div", class_="multiple-version-table")
 
+    table.attrs = {}
+    table.h2.attrs = {}
     # Prune Table Headers
     for header in table.thead.find_all("th"):
         header.attrs = {}
@@ -102,6 +107,25 @@ def prune_version_table(table: BeautifulSoup):
         button.decompose()
 
     return table
+
+
+def get_additional_prod_info(soup: BeautifulSoup):
+    """
+    Extract and prune the additional product information section from the page if there is one
+    """
+    section_header = soup.find("div", class_="columnsystem").get_text().strip()
+    if not section_header == "Additional product information":
+        return ""
+
+    result = ""
+    
+    for tag in soup.find_all("div", class_="columnsystem", limit=2):
+        prod_info_text = tag.get_text("\n").strip()
+        prod_info_text = remove_duplicate_newlines(prod_info_text)
+        result += prod_info_text + "\n"
+    
+    return "<p>" + result + "</p>\n"
+
 
 def get_metadata(soup, title):
     product = get_product_tag(soup, plc=True)
